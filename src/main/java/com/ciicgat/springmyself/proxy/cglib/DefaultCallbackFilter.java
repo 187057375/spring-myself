a4f90259637e464b013e5c735d0ca785802126b8
zhangtao0509 <zhangtao0509@126.com>
2018-10-12 15:34:21 +0800

    commit

--- src/main/java/com/ciicgat/springmyself/proxy/cglib/DefaultCallbackFilter.java
+++ src/main/java/com/ciicgat/springmyself/proxy/cglib/DefaultCallbackFilter.java
@@ -1,17 +1,12 @@
 package com.ciicgat.springmyself.proxy.cglib;
 
 import com.ciicgat.springmyself.annotation.Async;
-import com.ciicgat.springmyself.annotation.advice.Advice;
-import com.ciicgat.springmyself.annotation.advice.Aspect;
 import net.sf.cglib.proxy.Callback;
 import net.sf.cglib.proxy.CallbackFilter;
 import net.sf.cglib.proxy.NoOp;
 
 import java.lang.reflect.Method;
 
-/**
- * Created by sunguangzhu on 2018/8/23.
- */
 public class DefaultCallbackFilter implements CallbackFilter {
 
     public static final Callback[] callbacks = new Callback[]{
@@ -42,9 +37,7 @@ public class DefaultCallbackFilter implements CallbackFilter {
 
     @Override
     public int accept(Method method) {
-        if (method.isAnnotationPresent(Advice.class)){
-            return CallbackFilterEnum.INTERCEPTOR.getCode();
-        }else if (method.isAnnotationPresent(Async.class)){
+        if (method.isAnnotationPresent(Async.class)){
             return CallbackFilterEnum.ASYNC.getCode();
         }else {
             return CallbackFilterEnum.DEFAULT.getCode();
